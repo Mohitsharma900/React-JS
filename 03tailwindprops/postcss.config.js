c9e1e16d083838c528439df1d37620c713437c19
Mohit <168701475+Mohitsharma900@users.noreply.github.com>
2024-08-01 01:09:40 +0530

    React Foundation finished.

---
+++ 03tailwindprops/postcss.config.js
@@ -0,0 +1,14 @@
+export default {
+  mode: 'jit',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+
---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Cards from './Components/cards'
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <>
+      <h1 className='bg-red-400 text-black p-4 rounded-xl mb-4'>
+        Tailwind
+      </h1>
+      <Cards username="Aunty" btnText="Know more"/>
+      <Cards username="AuntyReturns" />
+    </>
+  )
+}
+
+export default App
